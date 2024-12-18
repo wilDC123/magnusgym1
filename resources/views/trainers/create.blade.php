4003e325dffe2632a7e29c27aacb3badaaf236d0
wilDC123 <diaswil521@gmail.com>
2024-12-17 22:51:38 -0400

    create.blade.php actualizado

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -1,40 +1,53 @@
 @extends('adminlte::page')
 
-@section('title', 'Agregar Membresía')
+@section('title', 'Agregar Entrenador')
 
 @section('content_header')
-    <h1>Agregar datos de la Membresía</h1>
+    <h1>Agregar datos del Entrenador</h1>
 @stop
 
 @section('content')
-    <form action="{{ route('memberships.store') }}" method="POST">
+    <form action="{{ route('trainers.store') }}" method="POST">
         @csrf
+        <div class="row">
+            <div class="col-md-8">
+                <div class="form-group">
+                    <label for="trainer_name">Nombre del Entrenador:</label>
+                    <input type="text" id="trainer_name" name="trainer_name" class="form-control" required>
+                </div>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="id_client">Cliente:</label>
-                    <select id="id_client" name="id_client" class="form-control" required>
-                        <option value="">Seleccionar Cliente</option>
-                        @foreach($clients as $client)
-                            <option value="{{ $client->id }}">{{ $client->client_name }}</option>
-                        @endforeach
-                    </select>
+                    <label for="phone">Teléfono:</label>
+                    <input type="text" id="phone" name="phone" class="form-control" required>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="id_plan">Plan:</label>
-                    <select id="id_plan" name="id_plan" class="form-control" required>
-                        <option value="">Seleccionar Plan</option>
-                        @foreach($plans as $plan)
-                            <option value="{{ $plan->id }}">{{ $plan->plan_name }}</option>
+                    <label for="email">Correo Electrónico:</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                    <label for="id_workshift">Horario de Trabajo:</label>
+                    <select id="id_workshift" name="id_workshift" class="form-control" required>
+                        <option value="">Seleccionar Horario</option>
+                        @foreach($workshifts as $workshift)
+                            <option value="{{ $workshift->id }}">{{ $workshift->workshift_name }}</option>
                         @endforeach
                     </select>
                 </div>
             </div>
         </div>
 
-        <a href="{{ route('memberships.index') }}" class="btn btn-secondary">Volver</a>
+        <a href="{{ route('trainers.index') }}" class="btn btn-secondary">Volver</a>
         <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
 @stop
